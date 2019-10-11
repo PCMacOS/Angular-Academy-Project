51b3ce3ba11c2246845ee6a73e486f0f0d08cd02
Panagiotis-Stephanos Makos <psmakos@hotmail.com>
2019-10-11 11:16:51 +0300

    Update table.component.ts

--- src/app/modules/table/table/table.component.ts
+++ src/app/modules/table/table/table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'angularAcademyProject-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
