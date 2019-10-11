28089d407960e551246c739d4254d32a5114540a
Panagiotis-Stephanos Makos <psmakos@hotmail.com>
2019-10-11 12:47:36 +0300

    Real time table update by Arry

--- src/app/modules/table/table/table.component.ts
+++ src/app/modules/table/table/table.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  table: any[][] = [['Dummy Title 1', 'Dummy Priority 1', 'Dummy Reporter 1', 'Dummy Date Created 1', 'Dummy Status 1'],
+  ['Dummy Title 2', 'Dummy Priority 2', 'Dummy Reporter 2', 'Dummy Date Created 2', 'Dummy Status 2'],
+  ['Dummy Title 3', 'Dummy Priority 3', 'Dummy Reporter 3', 'Dummy Date Created 3', 'Dummy Status 3']];
+
   constructor() { }
 
   ngOnInit() {
