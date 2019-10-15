04ffcf2907bd647691dddca60fc34d5e136a30d3
Panagiotis-Stephanos Makos <psmakos@hotmail.com>
2019-10-15 14:02:33 +0300

    Make routing for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableComponent } from './modules/table/table/table.component';
+import { PostFormComponent } from './modules/table/post-form/post-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TableComponent },
+  { path: 'post',      component: PostFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
