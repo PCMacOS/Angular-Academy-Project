38f2b5ded66d7480b39adb0fd03f5d35bff39474
Panagiotis-Stephanos Makos <psmakos@hotmail.com>
2019-10-18 14:01:54 +0300

    Fix navigation

--- src/app/modules/table/table.module.ts
+++ src/app/modules/table/table.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { TableComponent } from './table/table.component';
 import { PostFormComponent } from './post-form/post-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [TableComponent, PostFormComponent],
   imports: [
-    CommonModule, ReactiveFormsModule
+    CommonModule, ReactiveFormsModule, RouterModule
   ],
   exports: [TableComponent, PostFormComponent]
 })
